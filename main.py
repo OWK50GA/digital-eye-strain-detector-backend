5e4db53ae56c1dfc6bdbe18f808319f96df8d33e
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2025-09-09 06:45:25 +0100

    fixed port binding

--- main.py
+++ main.py
@@ -281,4 +281,5 @@ async def model_info():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
